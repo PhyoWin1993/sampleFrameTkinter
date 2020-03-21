54a9432ec6a6fecdfec02b5ed48cb17689932a76
PhyoWin1993 <mr.phyo.1571993mm@gmail.com>
2020-03-21 18:54:46 +0630

    sample frame Tkinter

---
+++ home.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+class Home:
+    def __init__(self,root,panels):
+        self.root = root
+        self.panels = panels
+        self.setUp()
+
+
+    def setUp(self):
+        button =  Button(self.root,text="To About ",command=lambda: self.changeScene("about")).pack()
+
+    def changeScene(self,scene):
+        self.panels[scene].tkraise()
